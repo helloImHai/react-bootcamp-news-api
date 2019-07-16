0c5f709fef795179953ba1fffbb83b81ca4e7a7f
helloImHai <1503.hai.nguyen@gmail.com>
2019-07-16 16:25:02 +0800

    Init commit

---
+++ client/src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {stories: []};
+
+  componentDidMount() {
+    fetch('http://localhost:3000/topstories')
+      .then(response => response.json())
+      .then(json => this.setState({stories: json}))
+      .catch(error => alert(error));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>The Latest Hacker News</h2>
+        <br/>
+        {
+          this.state.stories.map(story => {
+            const {id, title, url, by, score} = story;
+            return (
+                <div id={id}>
+                  <a href={url}><h3>{title}</h3></a>
+                  <p>upvotes: {score}</p>
+                  <p>- {by} -</p>
+                </div>
+              );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
